061795724d41623db4ff790ccfc4356cb0e26324
SeanTan7150 <xiuhawtan@gmail.com>
2024-03-25 14:41:43 +0800

    login form

--- src/DataIO/CredentialIO.java
+++ src/DataIO/CredentialIO.java
@@ -1,5 +1,16 @@
 package DataIO;
 
+import Classes.Credential;
+import static DataIO.DataIO.allCredential;
+
 public class CredentialIO {
+    public static Credential validateCredential(String userID, String password) {
+        Credential found = null;
+        for (Credential credential : allCredential) {
+            found = (credential.getUserID().equals(userID) && credential.getPassword().equals(password)) ? credential : null;
+            break;
+        }
+        return found;
+    } 
     // function checkUsername -> IC
 }
